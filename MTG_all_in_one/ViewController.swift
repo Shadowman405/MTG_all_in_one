7670f2bb78636ca0e39eccf14b1cf4fb6d56a621
m_mitin <maximus405@gmail.com>
2022-11-14 17:19:52 +0300

    Initial Commit

---
+++ MTG_all_in_one/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MTG_all_in_one
+//
+//  Created by Maxim Mitin on 14.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
