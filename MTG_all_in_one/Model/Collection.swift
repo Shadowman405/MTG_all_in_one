782b51725866e851d881e8e63a4d6efa394925df
m_mitin <maximus405@gmail.com>
2022-11-16 23:56:41 +0300

    new class for storage - in tests

---
+++ MTG_all_in_one/Model/Collection.swift
@@ -0,0 +1,16 @@
+//
+//  Collection.swift
+//  MTG_all_in_one
+//
+//  Created by Maxim Mitin on 16.11.22.
+//
+
+import Foundation
+
+
+class CardCollection: Codable {
+    let name: String = ""
+    let cards: [CardMTG] = []
+    
+    
+}
