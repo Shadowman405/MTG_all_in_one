a31b5579914958f7ed20c0f4989f194e9c0a19c5
m_mitin <maximus405@gmail.com>
2022-11-16 23:52:15 +0300

    add new Storage Manager

---
+++ MTG_all_in_one/Support Files/StorageManager.swift
@@ -0,0 +1,19 @@
+//
+//  StorageManager.swift
+//  MTG_all_in_one
+//
+//  Created by Maxim Mitin on 16.11.22.
+//
+
+import Foundation
+
+class StorageManager {
+    static let shared = NetworkManager()
+    
+    private let userDefaults = UserDefaults.standard
+    private let collectionKey = "collection"
+    
+    private init() {}
+    
+    
+}
