2d4c297e1a3ea7703c2eea210efe28cf2c06fabe
m_mitin <maximus405@gmail.com>
2022-12-02 21:40:36 +0300

    rework UIalertcontroller  - add extension and create default Alert controller with some actions

---
+++ MTG_all_in_one/Controller/AlertController.swift
@@ -0,0 +1,30 @@
+//
+//  AlertController.swift
+//  MTG_all_in_one
+//
+//  Created by Maxim Mitin on 2.12.22.
+//
+
+import UIKit
+
+extension UIAlertController {
+    static func createAlert(withTitle title: String, andMessage message:String) -> UIAlertController {
+        UIAlertController(title: title, message: message, preferredStyle: .alert)
+    }
+    
+    func action(completion: @escaping (String) -> Void) {
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let newValue = self.textFields?.first?.text else {return}
+            guard !newValue.isEmpty else {return}
+            completion(newValue)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        addAction(saveAction)
+        addAction(cancelAction)
+        addTextField{ txtField in
+            txtField.placeholder = "Collection Name"
+        }
+    }
+}
--- MTG_all_in_one/Controller/CollectionsTableViewController.swift
+++ MTG_all_in_one/Controller/CollectionsTableViewController.swift
@@ -66,3 +66,4 @@ class CollectionsTableViewController: UITableViewController {
         }
     }
 }
+
