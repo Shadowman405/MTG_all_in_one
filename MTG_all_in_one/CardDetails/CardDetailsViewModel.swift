26885f0f413fed08a15c4b4824f0afe00a3ac635
m_mitin <maximus405@gmail.com>
2023-01-28 22:24:01 +0300

    cardDetailsViewModel added

--- MTG_all_in_one/CardDetails/CardDetailsViewModel.swift
+++ MTG_all_in_one/CardDetails/CardDetailsViewModel.swift
@@ -9,5 +9,46 @@ import Foundation
 
 
 protocol CardDetailsViewModelProtocol {
+    var cardName: String {get}
+    var setName: String {get}
+    var cardRarity: String {get}
+    var manaCost: String {get}
+    var descriptionText: String {get}
+    var cardImage: String {get}
+    
+    init(card: CardMTG)
+}
+
+class CardDetailsViewModel: CardDetailsViewModelProtocol {
+    var cardName: String {
+        card.name
+    }
+    
+    var setName: String {
+        card.setName
+    }
+    
+    var cardRarity: String {
+        card.rarity
+    }
+    
+    var manaCost: String {
+        card.manaCost
+    }
+    
+    var descriptionText: String {
+        card.description
+    }
+    
+    var cardImage: String {
+        card.imageURL // need to create cardImageModelView - and work with new VM
+    }
+    
+    private let card: CardMTG
+    
+    required init(card: CardMTG) {
+        self.card = card
+    }
+    
     
 }
