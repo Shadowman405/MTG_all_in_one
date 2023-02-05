a18d706d5568eac071dbbabe358b44b97747a87b
Shadowman405 <maximus405@gmail.com>
2023-02-05 16:54:55 +0300

    start working on new VC

---
+++ MTG_all_in_one/CardsInCollection/CardsInCollectionViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  CardsInCollectionViewModel.swift
+//  MTG_all_in_one
+//
+//  Created by Maxim Mitin on 5.02.23.
+//
+
+import Foundation
+
+protocol CardsInCollectionViewModelProtocol {
+    
+}
+
+class CardsInCollectionViewModel: CardsInCollectionViewModelProtocol {
+    
+}
