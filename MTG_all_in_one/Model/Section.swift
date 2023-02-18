b5f3539727a4f461a0a252141ca6d9fdd9b4c178
Shadowman405 <maximus405@gmail.com>
2023-02-18 17:38:38 +0300

    add new Model
    trying to implement expandable sections logic

---
+++ MTG_all_in_one/Model/Section.swift
@@ -0,0 +1,20 @@
+//
+//  Section.swift
+//  MTG_all_in_one
+//
+//  Created by Maxim Mitin on 18.02.23.
+//
+
+import Foundation
+
+class Section {
+    let title: String
+    let duplicateCards: [CardMTG]
+    var isOpened = false
+    
+    init(title: String, duplicateCards: [CardMTG], isOpened: Bool = false) {
+        self.title = title
+        self.duplicateCards = duplicateCards
+        self.isOpened = isOpened
+    }
+}
