518fbcae2786198853a8a9a6dfb2e6609233fa34
Shadowman405 <maximus405@gmail.com>
2023-03-04 18:23:39 +0300

    add new VC for advanced search

---
+++ MTG_all_in_one/Advanced Search/AdvancedSearcViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AdvancedSearcViewController.swift
+//  MTG_all_in_one
+//
+//  Created by Maxim Mitin on 4.03.23.
+//
+
+import UIKit
+
+class AdvancedSearcViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
