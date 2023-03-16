894dfba27b3ec6d8c67d20e603a8fc0265e82c4d
Shadowman405 <maximus405@gmail.com>
2023-03-16 23:40:13 +0300

    add 2 new models for JSON parsing from API

---
+++ MTG_all_in_one/Model/Sets.swift
@@ -0,0 +1,12 @@
+//
+//  Sets.swift
+//  MTG_all_in_one
+//
+//  Created by Maxim Mitin on 16.03.23.
+//
+
+import Foundation
+
+class Sets: Codable {
+    
+}
---
+++ MTG_all_in_one/Model/Subtypes.swift
@@ -0,0 +1,12 @@
+//
+//  Subtypes.swift
+//  MTG_all_in_one
+//
+//  Created by Maxim Mitin on 16.03.23.
+//
+
+import Foundation
+
+class Subtypes: Codable {
+    
+}
