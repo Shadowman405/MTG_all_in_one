639b00c9c5eb662d1bae8b1f54f825f6135cc37c
Shadowman405 <maximus405@gmail.com>
2023-03-16 23:52:25 +0300

    Sets class - ok
    Looks like I will get only needed data

--- MTG_all_in_one/Model/Sets.swift
+++ MTG_all_in_one/Model/Sets.swift
@@ -8,5 +8,24 @@
 import Foundation
 
 class Sets: Codable {
+    let sets: [Set]
     
+    init(sets: [Set]) {
+        self.sets = sets
+    }
+}
+
+class Set: Codable {
+    let code, name, type: String
+    let releaseDate, block: String
+    let onlineOnly: Bool
+
+    init(code: String, name: String, type: String, releaseDate: String, block: String, onlineOnly: Bool) {
+        self.code = code
+        self.name = name
+        self.type = type
+        self.releaseDate = releaseDate
+        self.block = block
+        self.onlineOnly = onlineOnly
+    }
 }
