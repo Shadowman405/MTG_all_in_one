bfba0f1ddca81b25b30f6581efe6241ac1bbf484
Shadowman405 <maximus405@gmail.com>
2023-07-19 23:10:51 +0300

    update super types model

--- MTG_all_in_one/Model/Supertypes.swift
+++ MTG_all_in_one/Model/Supertypes.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+class Supertypes: Codable {
+    let supertypes: [String]
+
+    init(supertypes: [String]) {
+        self.supertypes = supertypes
+    }
+    
+    static func getAllSubtypes(from value: Any) -> [Supertypes]? {
+        guard let value = value as? [String:Any] else { return []}
+        guard let results = value as? [String:Any] else {return []}
+         //return results.compactMap { Subtypes(subtypes: $0)}
+        var arrRes = [[String]]()
+        for (_,val) in results {
+            arrRes.append(val as? [String] ?? [""])
+        }
+        return [Supertypes(supertypes: arrRes[0])]
+     }
+}
