1e91d5e089837089f17d64bd22be8b74f8d1fec5
Shadowman405 <maximus405@gmail.com>
2023-07-19 23:11:54 +0300

    update Formats model for adv. search

--- MTG_all_in_one/Model/Formats.swift
+++ MTG_all_in_one/Model/Formats.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+class Formats: Codable {
+    let formats: [String]
+
+    init(formats: [String]) {
+        self.formats = formats
+    }
+    
+    static func getAllSubtypes(from value: Any) -> [Formats]? {
+        guard let value = value as? [String:Any] else { return []}
+        guard let results = value as? [String:Any] else {return []}
+         //return results.compactMap { Subtypes(subtypes: $0)}
+        var arrRes = [[String]]()
+        for (_,val) in results {
+            arrRes.append(val as? [String] ?? [""])
+        }
+        return [Formats(formats: arrRes[0])]
+     }
+}
