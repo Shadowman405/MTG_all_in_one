1f5f565691ce2dd79937ee3606b840935c693c7b
Shadowman405 <maximus405@gmail.com>
2023-08-03 23:34:00 +0300

    trying to solve issue with duplicated cards

--- MTG_all_in_one/CardsCollection/CardsCollectionViewModel.swift
+++ MTG_all_in_one/CardsCollection/CardsCollectionViewModel.swift
@@ -6,10 +6,10 @@
 //
 
 import Foundation
+import Algorithms
 
 protocol CardCollectionViewModelProtocol {
     var cards: [CardMTG] { get }
-    
     
     func fetchCards(url: String, completion: @escaping () -> Void)
     func numberOfRows() -> Int
@@ -24,7 +24,8 @@ class CardCollectionViewModel: CardCollectionViewModelProtocol {
     
     func fetchCards(url: String, completion: @escaping () -> Void) {
         NetworkManager.shared.fetchCards(url: url) { cards in
-            self.cards = cards
+            let uniquedCards = Array(cards.uniqued())
+            self.cards = uniquedCards
             completion()
         }
 //        cards.filter {$0.imageURL == ""}.first?.imageURL = "https://preview.redd.it/fr7g5swymhc41.png?width=640&crop=smart&auto=webp&s=930c8edaa0acc0755c71c3d737840d08a9e9a0b0"
