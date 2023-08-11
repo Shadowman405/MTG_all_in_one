0716218c7046477fbc69cfea17ec828cab40b412
Shadowman405 <maximus405@gmail.com>
2023-08-11 22:43:17 +0300

    update united filtering

--- MTG_all_in_one/CardsCollection/CardsCollectionViewModel.swift
+++ MTG_all_in_one/CardsCollection/CardsCollectionViewModel.swift
@@ -26,11 +26,12 @@ class CardCollectionViewModel: CardCollectionViewModelProtocol {
         NetworkManager.shared.fetchCards(url: url) { cards in
             var uniquedCards = [CardMTG]()
             for card in cards {
-                if !uniquedCards.contains(where: {$0.imageURL != "" && $0.name == card.name}){
+//                if !uniquedCards.contains(where: {$0.imageURL != "" && $0.name == card.name}){
+//                    uniquedCards.append(card)
+//                }
                     uniquedCards.append(card)
-                }
             }
-            self.cards = uniquedCards
+            self.cards = uniquedCards.filter{$0.imageURL != ""}
             completion()
         }
 //        cards.filter {$0.imageURL == ""}.first?.imageURL = "https://preview.redd.it/fr7g5swymhc41.png?width=640&crop=smart&auto=webp&s=930c8edaa0acc0755c71c3d737840d08a9e9a0b0"
