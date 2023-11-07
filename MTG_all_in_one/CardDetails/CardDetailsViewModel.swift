5cc45bc34fbd124deda0ff9215b774b05b6273da
Shadowman405 <maximus405@gmail.com>
2023-11-07 23:36:23 +0300

    rework vie model

--- MTG_all_in_one/CardDetails/CardDetailsViewModel.swift
+++ MTG_all_in_one/CardDetails/CardDetailsViewModel.swift
@@ -15,39 +15,47 @@ protocol CardDetailsViewModelProtocol {
     var manaCost: String {get}
     var descriptionText: String {get}
     var cardImage: String {get}
-    var card: CardMTG {get}
+   // var card: CardMTG {get}
+    var card: Card {get}
     
-    init(card: CardMTG)
+    //init(card: CardMTG)
+    init(card: Card)
 }
 
 class CardDetailsViewModel: CardDetailsViewModelProtocol {
+    
     var cardName: String {
-        card.name
+        card.name ?? ""
     }
     
     var setName: String {
-        "Set name: " + card.setName
+        "Set name: " + (card.setName ?? "")
     }
     
     var cardRarity: String {
-        "Card rarity: " + card.rarity
+        "Card rarity: " + (card.rarity ?? "")
     }
     
     var cardImage: String {
-        card.imageURL
+        card.imageURL ?? ""
     }
     
     var manaCost: String {
-        card.manaCost
+        card.manaCost ?? ""
     }
     
     var descriptionText: String {
-        card.text
+        card.text ?? ""
     }
     
-    var card: CardMTG = CardMTG()
+    //var card: CardMTG = CardMTG()
+    var card: Card = Card(name: "", manaCost: "", cmc: 0, colors: [""], colorIdentity: [""], type: "", types: [""], subtypes: [""], rarity: "", cardSet: "", setName: "", text: "", toughness: "", layout: "", multiverseid: "", imageURL: "", rulings: [Ruling(date: "", text: "")], foreignNames: [ForeignName(name: "", text: "", type: "", flavor: "", imageURL: "", language: "", multiverseid: 0)], printings: [""], originalText: "", originalType: "", id: "", variations: [""])
     
-    required init(card: CardMTG) {
+//    required init(card: CardMTG) {
+//        self.card = card
+//    }
+    
+    required init(card: Card) {
         self.card = card
     }
     
