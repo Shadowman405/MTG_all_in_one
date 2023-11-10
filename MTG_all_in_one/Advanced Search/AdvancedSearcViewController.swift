d1d4b5c344fe2ae350ce2f767e4cd0ceac005d06
Shadowman405 <maximus405@gmail.com>
2023-11-10 16:40:28 +0300

    updates

--- MTG_all_in_one/Advanced Search/AdvancedSearcViewController.swift
+++ MTG_all_in_one/Advanced Search/AdvancedSearcViewController.swift
@@ -266,7 +266,7 @@ extension AdvancedSearcViewController: UISearchResultsUpdating {
             arrTypes = viewModel.typesMTG
             arrSupertypes = viewModel.supertypesMTG
             arrFormats = viewModel.formatsMTG
-            navigationController?.navigationBar.transparentNavigationBar()
+            //navigationController?.navigationBar.transparentNavigationBar()
         }
         
         func updateButtonsUI() {
